7ad0bc1d3856305b87d58229a4ee409879d88561
MAKUSref <tomaszmakowski002731@gmail.com>
2021-03-02 23:46:01 +0100

    add service worker

--- src/index.js
+++ src/index.js
@@ -2,4 +2,14 @@ import 'normalize.css/normalize.css';
 import "./scss/style.scss";
 import init from './js/start';
 
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', () => {
+		navigator.serviceWorker.register('dist/service-worker.js').then(registration => {
+			console.log('SW registered: ', registration);
+		}).catch(registrationError => {
+			console.log('SW registration failed: ', registrationError);
+		});
+	});
+}
+
 init();
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.js",
+  plugins: [
+    new WorkboxPlugin.GenerateSW({
+     // these options encourage the ServiceWorkers to get in there fast
+     // and not allow any straggling "old" SWs to hang around
+     clientsClaim: true,
+     skipWaiting: true,
+    }),
+  ],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
@@ -19,6 +29,6 @@ module.exports = {
     ]
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist')
+    contentBase: path.join(__dirname, '')
   }
 };
